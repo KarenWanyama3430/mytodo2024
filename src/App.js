5af99c05b44af6856640b38be1ece14e45e92f39
KarenWanyama3430 <karenwanyama3430@gmail.com>
2024-01-09 23:23:38 +0300

    changes to the todoMainWrapper

--- src/App.js
+++ src/App.js
@@ -5,16 +5,23 @@ import Goal from './Components/Pages/Goal/Goal';
 import Home from './Components/Pages/Home/Home';
 import Reward from './Components/Pages/Reward/Reward';
 import Todo from './Components/Pages/Todo/Todo';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import TodoMainWrapper from './Components/TodoMain/TodoMainWrapper'
 
 function App() {
   return (
     <div>
+      <Router>
       <Header/> 
       <Home/> 
       <Goal/>
       <Todo/>
       <Reward/>
   <Community/>
+  </Router>
+  <Routes>
+  <Route path="/todomainwrapper" element={<TodoMainWrapper />} />
+  </Routes>
     
     </div>
   );
